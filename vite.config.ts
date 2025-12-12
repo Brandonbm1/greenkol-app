6ea3ad96afd62db7b4fcd317c04ab211ffcf2672
Thaign <brandon.uan@gmail.com>
2025-12-11 23:21:34 -0500

    feature: add aliases for navigating on files

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,15 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    },
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@config': path.resolve(__dirname, './src/config'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@model': path.resolve(__dirname, './src/model'),
+      
+
+    }
   },
 })
